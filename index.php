3c9f2ee8cd8bc448772e9c4f0f442aa297989343
JEDIWONG <jediwzk@gmail.com>
2025-01-04 03:47:52 +0800

    Updates and Fixes

--- index.php
+++ index.php
@@ -1,18 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mamaku Vegetarian</title>
-    
-    
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home | Mamaku Vegetarian</title>
+        <link rel="stylesheet" href="./style/home.css">
+    </head>
+    <body>
+        <header>
+            <button class="menu">&#9776;</button>
+            <div class="home-logo">
+                <img src="./assets/images/Logo.svg" alt="Logo">
+                <h1>Mamaku Vegetarian</h1>
+            </div>
+            <nav> 
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="./view/menu.php">Our Menu</a></li>
+                    <li><a href="./view/cart.php">Cart</a></li>
+                    <li><a href="./view/register.php" class="btn signup">Sign Up</a></li>
+                    <li><a href="./view/login.php" class="btn login">Login</a></li>
+                </ul>
+            </nav>
+        </header>
 
-    <a href="./view/menu.php">Menu</a>
-    <a href="./view/login.php">Login</a>
-    <a href="./view/register.php">Register</a>
-    
-    
-</body>
+        <main>
+            <section class="news">
+                <h2>TASTE THE GREENERY WHILE</h2>
+                <h2>ENJOYING THE SCENERY</h2>
+                <p><a href="./view/menu.php">View All Menu</a></p>
+            </section>
+
+            <section class="about"> 
+                <h2>About Mamaku Vegetarian</h2>
+
+                <div class="box">
+                    <h3>100% Vegan Goodness</h3>
+                    <p>We provide a variety of vegetables as well as other soy-based foods.</p>
+                    <img src="./assets/images/tomato.svg" alt="100% Vegan Goodness">
+                </div>
+
+                <div class="box">
+                    <h3>Fresh from the market</h3>
+                    <p>We use the ingredients brought from the market, maintaining the freshness of the food.</p>
+                    <img src="./assets/images/Verified.svg" alt="Fresh from the market">
+                </div>
+
+                <div class="box">
+                    <h3>Flavourful & Nutritious</h3>
+                    <p>Our meals packed with taste and nutrients make healthy eating enjoyable.</p>
+                    <img src="./assets/images/nutrition.svg" alt="Flavourful & Nutritious">
+                </div>
+            </section>
+        </main>
+        
+        <footer>
+            <p>Copyright &copy; 2024. 烘培工坊 All Rights Reserved.</p>
+        </footer>
+    </body>
 </html>
